cdfc100932a693884d4f87a818e4698fa4a96bbc
khitun1 <81353419+khitun1@users.noreply.github.com>
2021-12-26 22:43:36 +0500

    Add files via upload

--- iPhone.js
+++ iPhone.js
@@ -7,3 +7,28 @@ document.body.onload = function()
             preloader.classList.add('done');
     }, 500);
 }
+
+
+function Price12(){
+    document.getElementById("Price12").style.display = "block";
+}
+
+function PriceMini(){
+    document.getElementById("PriceMini").style.display = "block";
+}
+
+function PriceXr(){
+    document.getElementById("PriceXr").style.display = "block";
+}
+
+function CloseP12(){
+    document.getElementById("Price12").style.display = "none";
+}
+
+function ClosePMini(){
+    document.getElementById("PriceMini").style.display = "none";
+}
+
+function ClosePXr(){
+    document.getElementById("PriceXr").style.display = "none";
+}
